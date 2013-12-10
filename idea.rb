1c5679b6f65e0e154efd9f098f14d320db3fc076
Elizabeth Glavin <eglavin@gmail.com>
2013-12-09 22:28:28 -0500

    edit complete

--- idea.rb
+++ idea.rb
@@ -11,30 +11,45 @@ class Idea
   def self.all
     raw_ideas.map do |data|
       new(data[:title], data[:description])
+    end
   end
-end
   def self.raw_ideas 
     database.transaction do |db|
       db['ideas'] || [] 
+    end
   end
-end
   def save
     database.transaction do |db|
       db['ideas'] ||= []
       db['ideas'] << {title: 'diet', description: 'pizza all the time'}
+    end
   end
-end
 
   def self.database
     @database ||= YAML::Store.new("ideabox")
-end
+  end
   def database
     Idea.database
   end
-
+  
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    new(raw_idea[:title], raw_idea[:description])
+  end
+  
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
   def self.delete(postion)
     database.transaction do 
       database['ideas'].delete_at(position)
+    end
   end
-end
 end
