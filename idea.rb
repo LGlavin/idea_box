d1f11a3fcaa6e09a38d00db70c5b42fd95faa993
Elizabeth Glavin <eglavin@gmail.com>
2013-12-09 21:56:51 -0500

    inital commit

---
+++ idea.rb
@@ -0,0 +1,40 @@
+require 'yaml/store'
+
+class Idea
+  attr_reader :title, :description
+
+  def initialize(title, description)
+    @title = title
+    @description = description
+  end
+  
+  def self.all
+    raw_ideas.map do |data|
+      new(data[:title], data[:description])
+  end
+end
+  def self.raw_ideas 
+    database.transaction do |db|
+      db['ideas'] || [] 
+  end
+end
+  def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: 'diet', description: 'pizza all the time'}
+  end
+end
+
+  def self.database
+    @database ||= YAML::Store.new("ideabox")
+end
+  def database
+    Idea.database
+  end
+
+  def self.delete(postion)
+    database.transaction do 
+      database['ideas'].delete_at(position)
+  end
+end
+end
